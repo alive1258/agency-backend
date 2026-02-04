d37a3149d9072237ce4358d9fa51831744dcba03
zamirul <javshazib@gmail.com>
2026-02-04 17:35:33 +0600

    blog catgory service added done

--- src/modules/blog-categories/blog-categories.module.ts
+++ src/modules/blog-categories/blog-categories.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BlogCategoriesService } from './blog-categories.service';
 import { BlogCategoriesController } from './blog-categories.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BlogCategory } from './entities/blog-category.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BlogCategory])],
   controllers: [BlogCategoriesController],
   providers: [BlogCategoriesService],
+  exports: [BlogCategoriesService],
 })
 export class BlogCategoriesModule {}
